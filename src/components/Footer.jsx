b30e925ae6b2137868c8bd40d329dca562171146
eyasinarafat3485-gif <eyasinarafat3485@gmail.com>
2026-05-02 18:44:28 +0600

    footer text color change

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
       <div className="max-w-7xl mx-auto px-5 md:px-15 grid grid-cols-1 md:grid-cols-3 gap-10">
         
         <div className="space-y-4">
-          <h3 className="text-red-500 text-lg font-semibold uppercase tracking-widest">Contact Info</h3>
+          <h3 className="text-orange-500 text-lg font-semibold uppercase tracking-widest">Contact Info</h3>
           <ul className="space-y-2 text-sm">
             <li className="hover:text-white text-gray-300 transition-colors">Email: support@skillsphere.com</li>
             <li className="hover:text-white text-gray-300  transition-colors">Phone: +880 1234-567890</li>
@@ -16,17 +16,17 @@ const Footer = () => {
         </div>
 
         <div className="flex flex-col items-start md:items-center space-y-4">
-          <h3 className="text-red-500 text-lg font-semibold uppercase tracking-widest">Social Links</h3>
+          <h3 className="text-orange-500 text-lg font-semibold uppercase tracking-widest">Social Links</h3>
           <div className="flex gap-6 text-2xl">
-            <Link href="/" className="text-gray-300  hover:text-red-500 transition-all duration-300 transform hover:-translate-y-1"><FaFacebook /></Link>
-            <Link href="/" className=" text-gray-300 hover:text-red-400 transition-all duration-300 transform hover:-translate-y-1"><FaTwitter /></Link>
-            <Link href="/" className="text-gray-300 hover:text-red-600 transition-all duration-300 transform hover:-translate-y-1"><FaLinkedin /></Link>
+            <Link href="/" className="text-gray-300  hover:text-orange-500 transition-all duration-300 transform hover:-translate-y-1"><FaFacebook /></Link>
+            <Link href="/" className=" text-gray-300 hover:text-orange-400 transition-all duration-300 transform hover:-translate-y-1"><FaTwitter /></Link>
+            <Link href="/" className="text-gray-300 hover:text-orange-600 transition-all duration-300 transform hover:-translate-y-1"><FaLinkedin /></Link>
             <Link href="/" className=" text-gray-300 hover:text-white transition-all duration-300 transform hover:-translate-y-1"><FaGithub /></Link>
           </div>
         </div>
 
         <div className="md:text-right space-y-4">
-          <h3 className="text-red-500 text-lg font-semibold uppercase tracking-widest">Legal</h3>
+          <h3 className="text-orange-500 text-lg font-semibold uppercase tracking-widest">Legal</h3>
           <ul className="space-y-2 text-sm">
             <li>
               <Link href="/terms" className="text-gray-300 hover:text-white transition-colors">Terms & Conditions</Link>
@@ -39,7 +39,7 @@ const Footer = () => {
       </div>
 
       <div className="max-w-7xl mx-auto pt-4 mt-5  border-t border-red-900/20 text-center text-[10px] uppercase tracking-[0.2em] text-gray-500">
-        <p className='text-gray-300'>&copy; {new Date().getFullYear()} <span className="text-red-500 ">SkillSphere</span>. All rights reserved.</p>
+        <p className='text-gray-300'>&copy; {new Date().getFullYear()} <span className="text-orange-500 ">SkillSphere</span>. All rights reserved.</p>
       </div>
     </footer>
   );
